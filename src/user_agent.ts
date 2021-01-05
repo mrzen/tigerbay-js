78a3d4b530ff4f5cd1e1168c38fe8cd70421a68b
LeoAdamek <leo@adamek.me>
2021-01-05 09:48:27 +0000

    Include lib version in UA

--- src/user_agent.ts
+++ src/user_agent.ts
@@ -1,6 +1,8 @@
+import { VERSION } from './version'
+
 /**
  * User-Agent header value
  * 
  * @todo Re-generate this when package is built
  */
-export const USER_AGENT = "tigerbay-js (+https://github.com/mrzen/tigerbay-js)"
+export const USER_AGENT = `tigerbay-js (+https://github.com/mrzen/tigerbay-js v${VERSION})`
