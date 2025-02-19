704af4a9712955d2bc0c10fc08cbceb0ddf47170
Leo Adamek <leo.adamek@mrzen.com>
2025-02-19 13:12:56 +0000

    Add new API calls for getting tour locations data

--- src/models/setup.ts
+++ src/models/setup.ts
@@ -1,4 +1,4 @@
-import { APIGroup } from "./common";
+import { APIGroup, LinkedObject } from "./common";
 
 export interface MarketingSource {
     Id: number
@@ -14,8 +14,20 @@ export interface MarketingSubsource {
     APIVisible: boolean
 }
 
+export interface Location extends LinkedObject {
+    Id: number
+    Name: string
+    Reference: string
+    Type: string
+    IsDomestic: boolean
+}
+
 export default class SetupAPI extends APIGroup {
     public async marketingSources(): Promise<MarketingSource[]> {
         return (await this.axios.get<MarketingSource[]>(`/setup/marketingSources`)).data
+    }
+
+    public async tourLocations(tourId: number): Promise<Location[]> {
+        return (await this.axios.get<Location[]>(`/setup/tours/${tourId}/locations`)).data
     }
 }
