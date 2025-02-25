cc27b69469410e783d787adb02e1abf0fbde4813
Leo Adamek <leo.adamek@mrzen.com>
2025-02-25 13:49:26 +0000

    Update payments API endpoint

--- src/models/payments.ts
+++ src/models/payments.ts
@@ -1,38 +1,22 @@
 import { APIGroup, LinkedObject } from "./common";
-import { Price } from "./reservations";
 
+export type PaymentStatus = 'Failed' | 'Authorised' | 'Pending' | 'Complete' |
+                            'UserCancelled' | 'Initialised'
+
+export type PaymentProvider = 'NotSet' | 'GlobalPayments' | 'Opayo' | 'Test' | 'Syntec' | 'DataCash'
 
 /**
  * Parameters to create a new payment
  */
 export interface CreatePaymentRequest {
-    Amount: PaymentAmount
-
-    AuthorizationCode?: string
-
+    TypeId?: number
+    Amount?: PaymentAmount
+    AuthorisationCode?: string
+    TransactionReference?: string
+    Token?: string
     Last4Digits?: string
-    Status?: "Completed" | "Pending" | "Failed" | "Authorised"
-
-    /**
-     * An optional payment reference
-     */
-    Reference?: string
-
-    /**
-     * URL to return the user to after making the payment
-     */
-    ReturnUrl?: string
-
-    /**
-     * ID of the customer contact to associate the payment with
-     */
-    ContactIdForPayment: number
-
-    /**
-     * Payment Type ID
-     */
-    SetupPaymentTypeId: number
-
+    Status?: PaymentStatus
+    Provider?: PaymentProvider
     ProviderData: Array<{key: string, value: string}>
 }
 
@@ -41,30 +25,31 @@ export interface PaymentAmount {
     Value: number
 }
 
-export interface CreatePaymentResponse extends LinkedObject {
+export interface PaymentType {
     Id: number
-    ReservationId: number
-    Url: string
-    Message: string
+    Name?: string
+    PaymentMethod?: 'Manual' | 'PaymentProvider'
 }
 
-export interface Payment extends CreatePaymentResponse {
-    Status: string
-    Reference: string
-    SetupPaymentCardId: number
-    CreatedDate: Date
-    CompletedDate: Date
-    Amount: Price
-    Charge: Price
-    Total: Price
-    CreatedByUserId: number
-    OwnerUserId: number
-    ParentID: number
-    PaymentProviderReference?: string
-    PaymentTypeReference?: string
-    Token?: string
+export interface Payment extends LinkedObject {
+    Id: number
+    CreatedDateTime: string
+    CreatedBy?: { Id?: number }
+    Type?: PaymentType
+    PaymentAmount: PaymentAmount
+    AuthorisationCode?: string
     TransactionReference?: string
     Last4Digits?: string
+    Status?: PaymentStatus
+    ProviderData?: Array<{key: string, value: string}>
+    Provider?: PaymentProvider
+    Reference?: string
+    CompletedDate?: string
+    IsScheduled?: boolean
+    ScheduledDate?: string
+    IsScheduledPrincipal?: boolean
+    IsScheduledPrincipalCandidate?: boolean
+    IsScheduledFinal?: boolean
 }
 
 /**
@@ -78,12 +63,28 @@ export class Api extends APIGroup {
      * @param reservationId ID of the reservation to add the payment to
      * @param payment Paramters for the payment
      */
-    public async create(reservationId: number, payment: CreatePaymentRequest): Promise<CreatePaymentResponse> {
-        return (await this.axios.post(`/sales/reservations/${reservationId}/payments`, payment)).data
+    public async create(reservationId: number, payment: CreatePaymentRequest): Promise<Payment> {
+        return (await this.axios.post(`/3/sales/reservations/${reservationId}/payments`, payment)).data
     }
 
+    /**
+     * 
+     * @param reservationId Reservation ID within which to find the payment
+     * @param id ID of the payment to find
+     * @returns {Promise<Payment>} Payment details
+     */
     public async find(reservationId: number, id: number): Promise<Payment> {
-        return (await this.axios.get<Payment>(`/sales/reservations/${reservationId}/payments/${id}`)).data
+        return (await this.axios.get<Payment>(`/3/sales/reservations/${reservationId}/payments/${id}`)).data
+    }
+
+    /**
+     * List Payments by Reservation
+     * 
+     * @param reservationId ID of Reservation to list payments for
+     * @returns {Promise<Payment[]>} List of payments made against the reservation
+     */
+    public async list(reservationId: number): Promise<Payment[]> {
+        return (await this.axios.get<Payment[]>(`/3/sales/reservations/${reservationId}/payments`)).data
     }
 
     /**
@@ -91,6 +92,8 @@ export class Api extends APIGroup {
      * 
      * @param reservationId ID of the reservation the payment is against
      * @param paymentId ID of the payment to finalize
+     * 
+     * @deprecated
      */
     public async finalize(reservationId: number, paymentId: string): Promise<Payment> {
         return (await this.axios.post(`/sales/reservations/${reservationId}/payments/${paymentId}/completionRequest`)).data
